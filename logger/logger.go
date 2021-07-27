f17a114302ca3273d004f9c7d0c343c6b420bc08
李寻欢 <lxh@cxh.cn>
2021-07-27 17:04:20 +0800

    :art: 优化日志的时间格式

--- logger/logger.go
+++ logger/logger.go
@@ -4,6 +4,7 @@ import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"os"
+	"time"
 )
 
 // Log 日志工具
@@ -14,9 +15,14 @@ func InitLogger() {
 	// 配置 sugaredLogger
 	writer := zapcore.AddSync(os.Stdout)
 
+	// 自定义时间输出格式
+	customTimeEncoder := func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString("[" + t.Format("2006-01-02 15:04:05.000") + "]")
+	}
+
 	// 格式相关的配置
 	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder   // 修改时间戳的格式
+	encoderConfig.EncodeTime = customTimeEncoder            // 修改时间戳的格式
 	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder // 日志级别使用大写显示
 	encoder := zapcore.NewConsoleEncoder(encoderConfig)
 
